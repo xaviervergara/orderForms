2e2872c75f53b2fc07fd131fc9fbe70578adcc5b
xaviervergara <xaviervergara00@gmail.com>
2024-08-28 01:37:28 -0300

    Render usedRange + numero total items + array filtrado sku

--- public/js/index.js
+++ public/js/index.js
@@ -18,18 +18,54 @@ document.getElementById('uploadForm').addEventListener('submit', async (e) => {
     const result = await response.json();
     console.log(result); // Verifica si la carga fue exitosa
 
-    // Una vez subido, puedes hacer el fetch para obtener el contenido
+    // Una vez subido, se hace el fetch para obtener el contenido
     const dataResponse = await fetch('http://localhost:8080/api/orderForm');
     const data = await dataResponse.json();
 
-    //Muestra solo el usedRange de la planilla
+    //Retorna solo el usedRange de la planilla
     const usedRange = data.filter((e) => {
       if (e[0] || e[1] !== null) {
         return e;
       }
     });
 
-    console.log(usedRange);
+    console.log(usedRange); //Ayuda para visualizar rapido el array completo
+
+    //Cantidad de producto
+
+    const cantidad_producto = usedRange.length - 1;
+
+    console.log(`La cantidad de productos es: ${cantidad_producto}`);
+
+    //Filtra solo los sku
+
+    const skus = usedRange.map((e) => {
+      return e[1];
+    });
+
+    skus.shift(); //?Le sacamos el primer item que es "sku", nos quedamos solo con los codigos
+
+    console.log(`Esto es skus: ${skus}`);
+
+    //* DOM RENDER
+
+    let form = document.getElementById('uploadForm');
+
+    form.innerHTML += `<div class="cant-item">Cantidad de items: ${cantidad_producto}</div>`;
+
+    let formContainer = document.getElementById('formContainer');
+    //i lo uso para colorear solo los 10 primeros 10 datos, (encabezado de la tabla)
+    let i = 0;
+    usedRange.forEach((element) => {
+      element.forEach((e) => {
+        if (i < 10) {
+          formContainer.innerHTML += `<div class="header">${e}</div>`;
+        } else {
+          formContainer.innerHTML += `<div class="cell">${e}</div>`;
+        }
+        i++;
+      });
+    });
   } catch (error) {
     console.error('Hubo un problema con la carga del archivo:', error);
   }
